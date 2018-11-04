978c729850d06d56592d59e9ba38b3de1a188edf
Hemory Phifer <hphife@yahoo.com>
2018-11-04 01:02:47 -0400

    Added the ability to see various comic books by their ID number

--- TreeHousePractice/Controllers/ComicBooksController.cs
+++ TreeHousePractice/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TreeHousePractice.Data;
 using TreeHousePractice.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,19 +12,23 @@ namespace TreeHousePractice.Controllers
 {
     public class ComicBooksController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private ComicBookRepository _comicBookrepository = null;
+
+        public ComicBooksController()
         {
-            return View();
+            _comicBookrepository = new ComicBookRepository();
         }
         
-        public ActionResult Detail()
+        public ActionResult Detail(int? id)
         {
-           
+            if (id == null)
+        {
+                return NotFound();
+        }
 
-
+            var comicBook = _comicBookrepository.GetComicBook((int)id);
        
-            return View();
+            return View(comicBook);
         }
     }
 }
--- TreeHousePractice/Data/ComicBookRepository.cs
+++ TreeHousePractice/Data/ComicBookRepository.cs
@@ -9,6 +9,7 @@ namespace TreeHousePractice.Data
         {
           new ComicBook()
     {
+        ID = 1,
         SeriesTitle = "The Amazing Spider-Man",
         IssueNumber = 700,
         DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
@@ -24,6 +25,8 @@ namespace TreeHousePractice.Data
     },
     new ComicBook()
     {
+        ID = 2,
+
         SeriesTitle = "The Amazing Spider-Man",
         IssueNumber = 657,
         DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -39,6 +42,7 @@ namespace TreeHousePractice.Data
     },
     new ComicBook()
     {
+        ID = 3,
         SeriesTitle = "Bone",
         IssueNumber = 50,
         DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
@@ -66,7 +70,7 @@ namespace TreeHousePractice.Data
                     break;
                 }
             }
-            S
+
             return comicBookToReturn;
         }
 
