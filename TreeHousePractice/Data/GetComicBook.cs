a6b4776d695aa86c6a6c9dbacb688c9151483cca
Hemory Phifer <hphife@yahoo.com>
2018-11-04 00:32:30 -0400

    Added Comic Book Repository

---
+++ TreeHousePractice/Data/GetComicBook.cs
@@ -0,0 +1,16 @@
+﻿using TreeHousePractice.Models;
+
+namespace TreeHousePractice.Data
+{
+    internal class GetComicBook
+    {
+        public GetComicBook()
+        {
+        }
+
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+    }
+}
